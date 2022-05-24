237724766b833566262d52093e59e1bbb500a572
b3nj4hb <benjalive12@gmail.com>
2022-05-24 13:23:45 -0500

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import morgan from 'morgan'
+
+const app = express();
+var cors = require('cors');
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+
+app.get('/',(req,res)=>{
+    res.send('Bienvenido a Node JS..!');
+});
+export default app;
